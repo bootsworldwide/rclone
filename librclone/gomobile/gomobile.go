cf68bdd04cb3ef75089092ea04c83a07f74f0677
Jonathan Boutsicaris <jboutsicaris@gmail.com>
2026-01-19 10:42:32 -0500

    update gomobile.go

--- librclone/gomobile/gomobile.go
+++ librclone/gomobile/gomobile.go
@@ -4,8 +4,27 @@ package gomobile
 import (
 	"github.com/rclone/rclone/librclone/librclone"
 
-	_ "github.com/rclone/rclone/backend/all" // import all backends
-	_ "github.com/rclone/rclone/lib/plugin"  // import plugins
+	_ "github.com/rclone/rclone/backend/all"
+	_ "github.com/rclone/rclone/cmd/about"
+	_ "github.com/rclone/rclone/cmd/bisync"
+	_ "github.com/rclone/rclone/cmd/backend"
+	_ "github.com/rclone/rclone/cmd/check"
+	_ "github.com/rclone/rclone/cmd/config"
+	_ "github.com/rclone/rclone/cmd/copy"
+	_ "github.com/rclone/rclone/cmd/delete"
+	_ "github.com/rclone/rclone/cmd/link"
+	_ "github.com/rclone/rclone/cmd/ls"
+	_ "github.com/rclone/rclone/cmd/move"
+	_ "github.com/rclone/rclone/cmd/purge"
+	_ "github.com/rclone/rclone/cmd/serve"
+	_ "github.com/rclone/rclone/cmd/size"
+	_ "github.com/rclone/rclone/cmd/sync"
+	_ "github.com/rclone/rclone/cmd/tree"
+	_ "github.com/rclone/rclone/cmd/version"
+	_ "github.com/rclone/rclone/fs/operations"
+	_ "github.com/rclone/rclone/fs/sync"
+	_ "github.com/rclone/rclone/fs/walk"
+	_ "github.com/rclone/rclone/lib/plugin"
 
 	_ "golang.org/x/mobile/event/key" // make go.mod add this as a dependency
 )
